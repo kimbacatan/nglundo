5d5424833b8b1fd9c730812f9adca44973d715f0
TekenCPS <131601746+TekenCPS@users.noreply.github.com>
2023-08-27 19:01:58 +0700

    Update world.h

--- proxy/world.h
+++ proxy/world.h
@@ -23,5 +23,36 @@ class world {
     std::vector<player> players{};
     player local{};
     bool connected{};
+
+int lastDroppedUid = 0;
+
+std::unordered_map<uint32_t, DroppedItem> objects;
+
+
+int droppedCount;
+				memcpy(&droppedCount, extended, 4);
+				memcpy(&lastDroppedUid, extended + 4, 4);
+				extended += 8;
+				//gt::send_log("Count: " + to_string(droppedCount) + " last uid: " + to_string(lastDroppedUid));
+
+				for (int i = 0; i < droppedCount; i++) {
+					DroppedItem item;
+					memcpy(&item.itemID, extended, 2);
+					memcpy(&item.pos.m_x, extended + 2, 4);
+					memcpy(&item.pos.m_y, extended + 6, 4);
+					memcpy(&item.count, extended + 10, 1);
+					memcpy(&item.flags, extended + 11, 1);
+					memcpy(&item.uid, extended + 12, 4);
+					extended += 16;
+					objects[item.uid] = item;
+				}
+				connected = true;
+			}
+		}
+		catch (const std::exception&)
+		{
+			gt::send_log("`4Unable To serialize this world");
+		}
+	}
 };
 
