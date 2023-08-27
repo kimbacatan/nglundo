043a76c59deca5ac2583d79cf1adc2267e391f86
TekenCPS <131601746+TekenCPS@users.noreply.github.com>
2023-08-27 19:15:59 +0700

    Update proxy.cpp

--- proxy/proxy.cpp
+++ proxy/proxy.cpp
@@ -11,6 +11,15 @@
 
 server* g_server = new server();
 
+bool isInside(int circle_x, int circle_y, int rad, int x, int y) {
+    // Compare radius of circle with distance
+    // of its center from given point
+    if ((x - circle_x) * (x - circle_x) + (y - circle_y) * (y - circle_y) <= rad * rad)
+        return true;
+    else
+        return false;
+}
+
 int main() {
 #ifdef _WIN32
     SetConsoleTitleA("Erza Proxy");
