c7927e0de026d6c73aa0d85f0cd1b71a7b2bc6ac
TekenCPS <131601746+TekenCPS@users.noreply.github.com>
2023-08-27 07:51:32 +0700

    Add files via upload

---
+++ proxy/gt.cpp
@@ -0,0 +1,15 @@
+#include "gt.hpp"
+#include "packet.h"
+#include "server.h"
+#include "utils.h"
+
+std::string gt::version = "4.34";
+std::string gt::flag = "id";
+bool gt::resolving_uid2 = false;
+bool gt::connecting = false;
+bool gt::in_game = false;
+bool gt::ghost = false;
+
+void gt::send_log(std::string text) {
+    g_server->send(true, "action|log\nmsg|" + text, NET_MESSAGE_GAME_MESSAGE);
+}
---
+++ proxy/proxy.cpp
@@ -0,0 +1,56 @@
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <thread>
+#include "enet/include/enet.h"
+#include "http.h"
+#include "NubiAuth.h"
+#include "android_device_id.h"
+#include "server.h"
+
+server* g_server = new server();
+
+int main() {
+#ifdef _WIN32
+    SetConsoleTitleA("Erza Proxy");
+#endif
+
+printf("Erza Proxy Authentication.\n\n");
+
+    std::string user_name = "";
+    printf("Enter License : "); std::cin >> user_name;
+
+
+    std::thread http(http::run, "127.0.0.1", "17191");
+    http.detach();
+
+
+    NubiAuth auth;
+    // api key from https://auth.nubizaserver.my.id/
+    auth.ApiKey = "ed651633ae61610b8a2b50c91f1830af42552if48mwbord";
+    // set device id
+    auth.device_id = get_device_id();
+    
+    enet_initialize();
+    if (auth.login(user_name)) {
+        printf("\nDevice Authorized\n");
+        
+    if (g_server->start()) {
+         
+   
+
+     printf("Erza Proxy is Running...\n");
+        printf("Erza Proxy Online!!\n\n");
+        printf("Now u can login growtopia");
+        while (true) {
+            g_server->poll();
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+ 
+        }
+    }  
+    }
+    
+    else
+        printf("Failed to connect Erza Proxy or Key Wrong!\n");
+};
