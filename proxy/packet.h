0d356a31c6ffb3bd05c1e3c4bdd35f5da1cda5c1
TekenCPS <131601746+TekenCPS@users.noreply.github.com>
2023-08-27 18:57:08 +0700

    Update packet.h

--- proxy/packet.h
+++ proxy/packet.h
@@ -1,7 +1,140 @@
 #pragma once
 #include <stdint.h>
+#include <iostream>
 
 #pragma pack(push, 1)
+
+struct GameUpdatePacket {
+	uint8_t type{}; //0
+	union {
+		uint8_t objtype{}; //1
+		uint8_t punchid;
+		uint8_t npctype;
+	};
+	union {
+		uint8_t count1{}; //2
+		uint8_t jump_count;
+		uint8_t build_range;
+		uint8_t npc_id;
+		uint8_t lost_item_count;
+	};
+	union {
+		uint8_t count2{}; //3
+		uint8_t animation_type;
+		uint8_t punch_range;
+		uint8_t npc_action;
+		uint8_t particle_id;
+		uint8_t gained_item_count;
+		uint8_t dice_result;
+		uint8_t fruit_count;
+	};
+	union {
+		int32_t netid{}; //4
+		int32_t effect_flags_check;
+		int32_t object_change_type;
+		int32_t particle_emitter_id;
+	};
+	union {
+		int32_t item{}; //8
+		int32_t ping_hash;
+		int32_t item_netid;
+		int32_t pupil_color;
+		int32_t tiles_length;
+	};
+	int32_t flags{}; //12 - 0xC
+	union {
+		float float_var{}; //16 - 0x10
+		float water_speed;
+		float obj_alt_count;
+	};
+	union {
+		int32_t int_data{}; //20 - 0x14
+		int32_t ping_item;
+		int32_t elapsed_ms;
+		int32_t delay;
+		int32_t tile_damage;
+		int32_t item_id;
+		int32_t item_speed;
+		int32_t effect_flags;
+		int32_t object_id;
+		int32_t hash;
+		int32_t verify_pos;
+		int32_t client_hack_type;
+	};
+	union {
+		float vec_x{}; //24 - 0x18
+		float pos_x;
+		float accel;
+		float punch_range_in;
+	};
+	union {
+		float vec_y{}; //28 - 0x1C
+		float pos_y;
+		float build_range_in;
+		float punch_strength;
+	};
+	union {
+		float vec2_x{}; //32 - 0x20
+		float dest_x;
+		float gravity_in;
+		float speed_out;
+		float velocity_x;
+		float particle_variable;
+		float pos2_x;
+		int hack_type;
+	};
+	union {
+		float vec2_y{}; //36 - 0x24
+		float dest_y;
+		float speed_in;
+		float gravity_out;
+		float velocity_y;
+		float particle_alt_id;
+		float pos2_y;
+		int hack_type2;
+	};
+	union {
+		float particle_rotation{}; //40 - 0x28
+		float npc_variable;
+	};
+	union {
+		uint32_t int_x{}; //44 - 0x2C
+		uint32_t item_id_alt;
+		uint32_t eye_shade_color;
+	};
+	union {
+		uint32_t int_y{}; //48 - 0x30
+		uint32_t item_count;
+		uint32_t eyecolor;
+		uint32_t npc_speed;
+		uint32_t particle_size_alt;
+	};
+	uint32_t data_size;
+	uint32_t data; //not officially a part of gameupdatepacket, thus sizeof(GameUpdatePacket) is 60 and not 56
+	void debug_print(const char* name) {
+		printf("---- start debug print ----\n");
+		printf("\tplayer: %s\n", name);
+		printf("type: %d\tobjtype: %d\tcount1: %d\tcount2: %d\n", type, objtype, count1, count2);
+		printf("netid: %d\titem: %d\tflags: %d\tfloat1: %0.2f\n", netid, item, flags, float_var);
+		printf("idata: %d\tvecx: %0.2f\tvecy: %0.2f\n", int_data, vec_x, vec_y);
+		printf("vec2x: %0.2f\tvec2y: %0.2f\tpart: %0.2f\n", vec2_x, vec2_y, particle_rotation);
+		printf("int_x: %d\tint_y: %d\tdata_size: %d\n", int_x, int_y, data_size);
+		if (fabsf(0.f - float_var) < 0.01f && float_var != 0.f)
+			printf("float var (small): %f (%d)\n", float_var, *(int*)&float_var);
+		if (fabsf(0.f - vec_x) < 0.01f && vec_x != 0.f)
+			printf("vecx (small): %f (%d)\n", vec_x, *(int*)&vec_x);
+		if (fabsf(0.f - vec_y) < 0.01f && vec_y != 0.f)
+			printf("vecy (small): %f (%d)\n", vec_y, *(int*)&vec_y);
+		if (fabsf(0.f - vec2_x) < 0.01f && vec2_x != 0.f)
+			printf("vec2x (small): %f (%d)\n", vec2_x, *(int*)&vec2_x);
+		if (fabsf(0.f - vec2_y) < 0.01f && vec2_y != 0.f)
+			printf("vec2y (small): %f (%d)\n", vec2_y, *(int*)&vec2_y);
+		if (fabsf(0.f - particle_rotation) < 0.01f && particle_rotation != 0.f)
+			printf("part (small): %f (%d)\n", particle_rotation, *(int*)&particle_rotation);
+		printf("---- end debug print ----\n");
+	}
+};
+
 struct gameupdatepacket_t {
 	uint8_t m_type;
 	uint8_t m_netid;
