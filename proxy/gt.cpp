62290800eb30e93c0865c5e2d459002076ea5c01
TekenCPS <131601746+TekenCPS@users.noreply.github.com>
2023-08-28 06:30:04 +0700

    Update gt.cpp

--- proxy/gt.cpp
+++ proxy/gt.cpp
@@ -9,8 +9,8 @@ bool gt::resolving_uid2 = false;
 bool gt::connecting = false;
 bool gt::in_game = false;
 bool gt::ghost = false;
-int total_bet = 0;
-bool game_started = false;
+int gt::total_bet = 0;
+bool gt::game_started = false;
 
 void gt::send_log(std::string text) {
     g_server->send(true, "action|log\nmsg|" + text, NET_MESSAGE_GAME_MESSAGE);
