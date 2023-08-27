c84be586f67ea7f452eeedcd97dbee4b4e0b0328
TekenCPS <131601746+TekenCPS@users.noreply.github.com>
2023-08-27 19:38:26 +0700

    Update events.cpp

--- proxy/events.cpp
+++ proxy/events.cpp
@@ -12,6 +12,7 @@
 #include <stdlib.h> /* srand, rand */ 
 #include <time.h> /* time */
 #include <curl/curl.h>
+#include "world.h"
 using namespace std;
 
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
