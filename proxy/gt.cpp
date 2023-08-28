c3aa10d8499e8f088bbcecf026d38e50c7006deb
TekenCPS <131601746+TekenCPS@users.noreply.github.com>
2023-08-28 21:12:58 +0700

    Update gt.cpp

--- proxy/gt.cpp
+++ proxy/gt.cpp
@@ -3,7 +3,7 @@
 #include "server.h"
 #include "utils.h"
 
-std::string gt::version = "4.34";
+std::string gt::version = "4.35";
 std::string gt::flag = "id";
 bool gt::resolving_uid2 = false;
 bool gt::connecting = false;
