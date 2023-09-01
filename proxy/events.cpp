4f97b62dee965d3655332174f088020453799c50
TekenCPS <131601746+TekenCPS@users.noreply.github.com>
2023-09-01 09:42:46 +0700

    Update events.cpp

--- proxy/events.cpp
+++ proxy/events.cpp
@@ -1793,7 +1793,68 @@ bool events::in::gamemessage(std::string packet) {
     return false;
 }
 
+bool events::in::OnChangeObject(gameupdatepacket_t* packet) {
+if (packet->m_vec_x == 0 && packet->m_vec_y == 0) {
+                            if (packet->m_player_flags == m_world.local.netid) {
+                                auto object = m_world.objects.find(packet->m_int_data);
+                                if (object->second.itemID != 112) {
+                                    auto s_items_ptr = &local_player.inventory.items;
+                                    int buffer = local_player.inventory.getObjectAmountToPickUpUnsafe(object->second);
+                                    if (!local_player.inventory.doesItemExistUnsafe(object->second.itemID)) { //haven't tested yet.
+                                        Item item;
+                                        item.id = object->second.itemID;
+                                        item.count = buffer;
+                                        item.type = 0;
+                                        s_items_ptr->operator[](item.id) = item;
+                                    }
+                                   
+                                    else s_items_ptr->operator[](object->second.itemID).count += buffer;
 
+                                    if (gt::game_started == true) {
+                                        if (object->second.itemID == 242) {
+                                            gt::total_bet += buffer;
+                                        }
+                                        if (object->second.itemID == 1796) {
+                                            gt::total_bet += (buffer * 100);
+                                        }
+                                    }
+                                    //cout << "degisen: " << buffer << endl;
+                                    if (s_items_ptr->operator[](242).count >= 100) {
+                                        gameupdatepacket_t goto{ 0 };
+                                        goto.m_type = PACKET_ITEM_ACTIVATE_REQUEST;
+                                        goto.m_int_data = 242;
+                                        g_server->send(false, NET_MESSAGE_GAME_PACKET, (uint8_t*)&goto, sizeof(gameupdatepacket_t));
+                                     
+                                    }
+                                }
+                                else {
+                                    local_player.gems_balance += object->second.count;
+                                }
+
+                            }
+                            m_world.objects.erase(packet->m_int_data);
+                        }
+                        else {
+                            if (packet->m_player_flags == -1) {
+                                DroppedItem item;
+                                item.itemID = packet->m_int_data;
+                                item.pos = vector2_t(packet->m_vec_x, packet->m_vec_y);
+                                item.count = uint32_t(packet->m_struct_flags);
+                                item.flags = 0; //set this to what??
+                                item.uid = ++m_world.lastDroppedUid;
+                                m_world.objects[item.uid] = item;
+                            }
+                            else if (packet->m_player_flags == -3) {
+                                auto obj = m_world.objects.find(packet->m_item);
+                                if (obj != m_world.objects.end()) {
+                                    obj->second.itemID = packet->m_int_data;
+                                    obj->second.pos = vector2_t(packet->m_vec_x, packet->m_vec_y);
+                                    obj->second.count = uint32_t(packet->m_struct_flags);
+                                }
+                            }
+                        }
+	return false;
+}
 
 bool events::in::sendmapdata(gameupdatepacket_t* packet) {
     g_server->m_world = {};
