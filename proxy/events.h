b9b99e5da773daccb3aee224f48445b5f5b200b8
TekenCPS <131601746+TekenCPS@users.noreply.github.com>
2023-09-02 10:39:52 +0700

    Update events.h

--- proxy/events.h
+++ proxy/events.h
@@ -32,7 +32,6 @@ namespace events {
         bool sendmapdata(gameupdatepacket_t* packet);
         bool state(gameupdatepacket_t* packet);
         bool tracking(std::string packet);
-bool OnChangeObject(gameupdatepacket_t* packet);
     }; // namespace in
 };     // namespace events
 
