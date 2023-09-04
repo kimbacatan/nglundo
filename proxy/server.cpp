b08d38961cb71abafa2baa6c296fb6934ff3e1ab
TekenCPS <131601746+TekenCPS@users.noreply.github.com>
2023-09-04 20:44:09 +0700

    Update server.cpp

--- proxy/server.cpp
+++ proxy/server.cpp
@@ -516,3 +516,72 @@ void server::send(bool client, std::string text, int32_t type) {
 
 
     
+void server::MoveXY(int x, int y, int lowx, int lowy) {
+    try {
+        auto world = g_server->m_world;
+
+
+        vector<pair<int, int>> path = pf.aStar(g_server->Local_Player.pos.m_x / 32, g_server->Local_Player.pos.m_y / 32, x, y);
+
+        if (path.size() > 0) {
+            if (path.size() < 150)
+                for (auto& p : path) {
+                    gameupdatepacket_t packet{ 0 };
+                    packet.m_type = PACKET_STATE;
+                    packet.m_int_data = 6326;
+                    packet.m_vec_x = p.first * 32;
+                    packet.m_vec_y = p.second * 32;
+                    packet.m_state1 = p.first;
+                    packet.m_state2 = p.second;
+                    packet.m_packet_flags = UPDATE_PACKET_PLAYER_MOVING_RIGHT;
+                    g_server->send(false, NET_MESSAGE_GAME_PACKET, (uint8_t*)&packet, sizeof(gameupdatepacket_t));
+
+                    std::this_thread::sleep_for(std::chrono::milliseconds(2));
+                }
+            else {
+                for (std::size_t i = 0; i < path.size(); i += 2) {
+                    gameupdatepacket_t packet{ 0 };
+                    packet.m_type = PACKET_STATE;
+                    packet.m_int_data = 6326;
+                    packet.m_vec_x = path[i].first * 32;
+                    packet.m_vec_y = path[i].second * 32;
+                    packet.m_state1 = path[i].first;
+                    packet.m_state2 = path[i].second;
+                    packet.m_packet_flags = UPDATE_PACKET_PLAYER_MOVING_RIGHT;
+                    g_server->send(false, NET_MESSAGE_GAME_PACKET, (uint8_t*)&packet, sizeof(gameupdatepacket_t));
+
+                    std::this_thread::sleep_for(std::chrono::milliseconds(2));
+                }
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(2));
+
+
+
+            variantlist_t lost{ "OnSetPos" };
+            vector2_t pos;
+            pos.m_x = x * 32;
+            pos.m_y = y * 32;
+            lost[1] = pos;
+            g_server->send(true, lost, g_server->Local_Player.netid, -1);
+
+            variantlist_t notif{ "OnAddNotification" };
+            notif[2] = "`2Traveling `9" + to_string(path.size()) + " `2Block";
+            notif[4] = 0;
+            g_server->send(true, notif, -1, -1);
+        }
+        else {
+            variantlist_t notif{ "OnAddNotification" };
+            notif[2] = "`4Traveling Not Possible";
+            notif[4] = 0;
+            g_server->send(true, notif, -1, -1);
+        }
+    }
+    catch (exception ex)
+    {
+        variantlist_t notif{ "OnAddNotification" };
+        notif[2] = "`8Something Goes Wrong";
+        notif[4] = 0;
+        g_server->send(true, notif, -1, -1);
+    }
+ 
+}
